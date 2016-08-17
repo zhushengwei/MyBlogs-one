91e2a54cfffef118cb53711cd391e8d420bf5cb3
zhushengwei <15918660832@139.com>
2016-08-17 08:32:15 +0800

    two

--- app/main/views.py
+++ app/main/views.py
@@ -1,25 +1,32 @@
 from . import main
 from flask import render_template, request, current_app
-from ..models import Article, ArticleType
+from ..models import Article, ArticleType, article_types
 
 @main.route('/')
 def index():
     page = request.args.get('page', 1, type=int)
-    pagination = Article.query.orger_by(Article.create_time.desc()).paginate(
-        page, per_page=current_app.confing['ARTICLES_PER_PAGE'],
-        error_out=False
-    )
+    pagination = Article.query.order_by(Article.create_time.desc()).paginate(
+            page, per_page=current_app.config['ARTICLES_PER_PAGE'],
+            error_out=False)
     articles = pagination.items
-    return render_template('index.html', ArticleType=ArticleType, articles=articles,
-                           pagination=pagination)
+    return render_template('index.html', ArticleType=ArticleType, article_types=article_types,
+                           articles=articles, pagination=pagination, endpoint='.index')
 
 
-@main.route('/article-types/<int:id>')
+@main.route('/article-detials/<int:id>')
+def articleDetails(id):
+    article = Article.query.get_or_404(id)
+    return render_template('article_detials.html', ArticleType=ArticleType,
+                           article_types=article_types, article=article)
+
+
+@main.route('/article-types/<int:id>/')
 def articleTypes(id):
     page = request.args.get('page', 1, type=int)
     pagination = ArticleType.query.get_or_404(id).articles.order_by(
-        Article.create_time.desc()).paginate(page,
-        per_page=current_app['ARTICLES_PER_PAGE'], error_out=False)
+            Article.create_time.desc()).paginate(
+            page, per_page=current_app.config['ARTICLES_PER_PAGE'],
+            error_out=False)
     articles = pagination.items
-    return render_template('index.html', ArticleType=ArticleType, articles=articles,
-                           pagination=pagination, endpoint='.articleType', id=id)
+    return render_template('index.html', ArticleType=ArticleType, article_types=article_types,
+                           articles=articles, pagination=pagination, endpoint='.articleTypes', id=id)
